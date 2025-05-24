20f3d58ea93b4c57547162fcead4491a72c8eaea
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-24 10:13:18 +0800

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:calculator/screens/calculator_screen.dart';
+
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorScreen(),
+    );
+  }
+}
