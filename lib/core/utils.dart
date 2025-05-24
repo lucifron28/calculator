d49d685983fc69c655e0b5b8cfba57af2b1c4d4b
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-24 10:57:42 +0800

    added input sanitizer

---
+++ lib/core/utils.dart
@@ -0,0 +1,15 @@
+import 'package:math_expressions/math_expressions.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger();
+
+Expression? formatToExpression(String input) {
+  try {
+    ExpressionParser p = GrammarParser();
+    String sanitizedInput = input.replaceAll('x', '*').replaceAll('÷', '/');
+    return p.parse(sanitizedInput);
+  } catch (e) {
+    logger.e("Invalid input: $e");
+    return null;
+  }
+}
