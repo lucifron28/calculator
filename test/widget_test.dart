f1626d4412a78586dfc9d80600daac09464a532c
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-25 10:29:18 +0800

    fix errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculator/main.dart';
+import 'package:calculator/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CalculatorApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
