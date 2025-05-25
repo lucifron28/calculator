ab4b33856bf97d5d2dc97df90958999a35b10590
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-25 10:42:47 +0800

    add styling for display

--- lib/widgets/display.dart
+++ lib/widgets/display.dart
@@ -7,12 +7,20 @@ class CalculatorDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(),
-        color: Colors.black,
-        ),
-      child: Text(text),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.14,
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          ),
+        child: FittedBox(
+          alignment: Alignment.centerRight,
+          child: Text(text),
+          ),
+      ),
     );
   }
 }
